91417af8452a4a631c19b6c46490cd76eb23dad5
Ayrton Araújo <root@ayrtonaraujo.net>
2013-07-21 23:44:54 -0400

    Finished

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function CounterCtrl($scope) {
+    $scope.count = 0;
+    $scope.discos = prompt("Qual a quantidade de discos?","2");
+    $scope.init = function() {
+        $scope.count = Math.pow(2,$scope.discos) - 1;
+    }
 
+    $scope.fall = function() {
+        $scope.count -= 1;
+    }
 }
